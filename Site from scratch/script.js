f081ab70b3084c86a49f82326d3ad198dac40e86
Zeliza2004 <48289233+Zeliza2004@users.noreply.github.com>
2020-11-09 11:22:11 +0100

    Add files via upload

---
+++ Site from scratch/script.js
@@ -0,0 +1,9 @@
+var typed3 = new Typed('.typed', {
+    strings: ["Passionnée par l'univers du web et dotée d'une grande curiosité, je suis en reconversion professionnelle et suis une formation de développeuse web et web mobile.",
+        "J'aime la programmation et les nouvelles technologies.",
+        "Je recherche actuellement une entreprise pour perfectionner mes connaissances et mes compétences au cours d'un stage d'un mois."],
+    typeSpeed: 30,
+    backSpeed: 0,
+    smartBackspace: true, // this is a default
+    loop: false
+});
